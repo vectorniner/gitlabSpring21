ccbafcd034a8fd57561171d58d90fa682f0fa9e9
bananaram88 <82845948+bananaram88@users.noreply.github.com>
2021-04-21 15:43:10 -0700

    Update rpgGame.c

--- rpgGame.c
+++ rpgGame.c
@@ -119,6 +119,47 @@ int main(int argc, char *argv[])
 			{
 				while(choice != 99)
 				{
+					int i, j, n=10;
+
+    					for(i=n/2; i<=n; i+=2)
+ 					{
+        					for(j=1; j<n-i; j+=2)
+        					{
+            						printf(" ");
+       						}
+
+        					for(j=1; j<=i; j++)
+        					{
+            						printf("*");
+        					}
+
+        					for(j=1; j<=n-i; j++)
+        					{
+            						printf(" ");
+        					}
+	
+        					for(j=1; j<=i; j++)
+        					{
+            						printf("*");
+        					}
+
+        					printf("\n");
+    					}
+
+    					for(i=n; i>=1; i--)
+    					{
+        					for(j=i; j<n; j++)
+        					{
+            						printf(" ");
+        					}
+
+        					for(j=1; j<=(i*2)-1; j++)
+        					{
+            						printf("*");
+        					}
+
+        						printf("\n");
+   					}
 					puts("you open the door and find ........");
 					scanf("%d",&choice);
 				}
