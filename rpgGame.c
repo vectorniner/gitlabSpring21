a9da1d7659aa7dc661562f59c422b58b58c4d23b
lboado <lboado1@toromail.csudh.edu>
2021-04-21 15:23:17 -0700

    Added room 3

--- rpgGame.c
+++ rpgGame.c
@@ -45,7 +45,8 @@ int main(int argc, char *argv[])
 			{
 				while(choice != 99)
 				{
-					puts("you open the door and find ........");
+					puts("you open the door and find a mysterious man saying: 'Wendy, darling, Light of my Life! I'm not gonna hurt ya \n");
+					puts("He looks at you menancingly and starts to run to you with a knife, there are multiple doors behind you which door do you pick? \n you may quit anytime by selecting option:99");
 					scanf("%d",&choice);
 				}
 				break;
